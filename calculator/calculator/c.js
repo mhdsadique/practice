4a7e16c5a32622eb03c187cdbf333f4425b9dab7
Muhammed sadique <mhdsadique8@gmail.com>
2023-07-31 12:11:17 +0530

    data

--- calculator/calculator/c.js
+++ calculator/calculator/c.js
@@ -1,28 +1,5 @@
-/*
-// let data=document.querySelectorAll(".bott>div")
-// for(let i=0;i<data.length;i++){
-//     data[i].addEventListener("click",clickme)
-// }
-// let bag=""
-// function clickme(event){
-//     let valu=event.target.innerText
-//     let dis=   document.getElementById("display")
-//     if(valu=="C"){
-//         bag=""
-//     }else if(valu=="="){
-//        dis.innerText= eval(bag)
-//     }else{
-//         bag+=valu
-//         dis.innerText= bag
-//     }
-//     console.log(valu)
-// }
-*/
-
 
 const apps= document.querySelectorAll("#bott>div")
-// addEventListener
-console.log(apps)
  for(let q=0;q<apps.length;q++){
     apps[q].addEventListener("click",calcu)
  }
