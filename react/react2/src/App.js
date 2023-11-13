51de547f25e6b152c4edcf1b32362d2f1f188d91
Muhammed sadique <mhdsadique8@gmail.com>
2023-11-13 21:50:20 +0530

    newdata

---
+++ react/react2/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Home } from './component/home';
+function App() {
+  return (
+    <div className="App">
+    <div>
+      <h1>Helle world</h1>
+      <Home />
+    </div>
+    </div>
+  );
+}
+
+export default App;
